1593008cb9523d1ea8f486d1f34654526813d8a2
Fabrice Tobler <fabrice@tobler.dreipol.ch>
2022-05-23 22:21:56 +0200

    Add express, fix styling breakpoints

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import express from 'express';
+import * as path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, './source/public')));
+
+
+
+app.get("/:note/:some", (req, res) => {
+    res.sendFile(path.join(__dirname, './source/public/index.html'))
+})
 
 app.listen(port, () => {
     // eslint-disable-next-line no-console
